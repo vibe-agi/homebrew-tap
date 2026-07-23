9bfa64deab2a2bf7fefdfc1a64b4c1fc7ad17fc2
null <null@null.com>
2026-07-23 13:29:18 +0800

    s3disk: update to v0.1.0-rc.3
    Signed-off-by: null <null@null.com>

--- Formula/s3disk.rb
+++ Formula/s3disk.rb
@@ -5,23 +5,23 @@ class S3disk < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/vibe-agi/s3disk/releases/download/v0.1.0-rc.2/s3disk-v0.1.0-rc.2-darwin-arm64.tar.gz"
-      sha256 "93d02484d63c4a53a1978e35442a24ac510e26d9b71f4fc260452eea548d4a22"
+      url "https://github.com/vibe-agi/s3disk/releases/download/v0.1.0-rc.3/s3disk-v0.1.0-rc.3-darwin-arm64.tar.gz"
+      sha256 "802ed30c71dc4f9b851b53671c548b3c3ed96977d07707665a9c9ef090d699e1"
     end
     on_intel do
-      url "https://github.com/vibe-agi/s3disk/releases/download/v0.1.0-rc.2/s3disk-v0.1.0-rc.2-darwin-amd64.tar.gz"
-      sha256 "6cefe27ca77ad07c1229f5431a8c872837f94f75969d3d84e1e159044267feef"
+      url "https://github.com/vibe-agi/s3disk/releases/download/v0.1.0-rc.3/s3disk-v0.1.0-rc.3-darwin-amd64.tar.gz"
+      sha256 "fd6381c93e8a1006c2c33a855054b096f6e9c44c649ade8b09d2fbcfccb84834"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/vibe-agi/s3disk/releases/download/v0.1.0-rc.2/s3disk-v0.1.0-rc.2-linux-arm64.tar.gz"
-      sha256 "1f25d5a62d09ab3775b1c8510ce3a58b7727a0b3f8f40c4f29e6b83f55a98453"
+      url "https://github.com/vibe-agi/s3disk/releases/download/v0.1.0-rc.3/s3disk-v0.1.0-rc.3-linux-arm64.tar.gz"
+      sha256 "2abc7154263c786553bc5400eb0a7bc0b7829f4f0634460331e9c26e2557d95e"
     end
     on_intel do
-      url "https://github.com/vibe-agi/s3disk/releases/download/v0.1.0-rc.2/s3disk-v0.1.0-rc.2-linux-amd64.tar.gz"
-      sha256 "63f4595233e4a38ad1dc4f4aefa854b4d0f25327904bc9b554075a97741476c1"
+      url "https://github.com/vibe-agi/s3disk/releases/download/v0.1.0-rc.3/s3disk-v0.1.0-rc.3-linux-amd64.tar.gz"
+      sha256 "3e9e81e87a919399baf0d635c4a0bf6d71ebedcd7311d7909ce28d9332111d34"
     end
   end
 
