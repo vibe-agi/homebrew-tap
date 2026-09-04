631d057fa946f74e7ab6b9ec8d74b6ecf3af9376
xujinzheng <xujinzheng@gmail.com>
2026-09-04 21:53:20 +0800

    vibermate: update to 0.1.1

--- Casks/vibermate.rb
+++ Casks/vibermate.rb
@@ -1,6 +1,6 @@
 cask "vibermate" do
-  version "0.1.0"
-  sha256 "13fc008a8642b671f584bc286d83055f1fda192f01dcd63b7c29e2e28f2accd5"
+  version "0.1.1"
+  sha256 "bbbc0fb2c5a7ee146141c7abc475e6d691d0b72e6bd12b207a84acc032ca4e24"
 
   url "https://github.com/vibe-agi/vibermate/releases/download/v#{version}/ViberMate_#{version}_universal.dmg"
   name "ViberMate"
