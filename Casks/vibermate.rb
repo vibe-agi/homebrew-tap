d2950307c1e9b603c17627d5e978dbf3b81e4562
null <null@null.com>
2026-09-07 00:34:38 +0800

    vibermate: update cask to 0.1.8

--- Casks/vibermate.rb
+++ Casks/vibermate.rb
@@ -1,6 +1,6 @@
 cask "vibermate" do
-  version "0.1.7"
-  sha256 "fd92791b18e71a4fcb3417adc1e6bfd944532c068838f09c39606f0de436599d"
+  version "0.1.8"
+  sha256 "ccc01e60aedbac72204933baf26ea02633be2540e83c51e6d5f8a127fa3efe90"
 
   url "https://github.com/vibe-agi/vibermate/releases/download/v#{version}/ViberMate_#{version}_universal.dmg"
   name "ViberMate"
